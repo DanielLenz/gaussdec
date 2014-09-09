243a4368b8a79fc6c9d03bcf83b7721d09cb2893
Daniel Lenz <dlenz.bonn@gmail.com>
2014-09-09 10:42:53 +0200

    Minor bugfixes.

--- fit_ATT.py
+++ fit_ATT.py
@@ -12,20 +12,21 @@ from astropy.io import fits
 from fitting import fit_spectrum, make_multi_gaussian_model, default_p
 
 
-def fit_file(filename, p):
+def fit_file(args):
 
-    outname = os.path.join(os.path.basename(filename), '_fits.gzjs')
+    filename, p = args
+    outname = os.path.splitext(filename)[0] + '_fits.gzjs'
 
     if not os.path.exists(outname):
 
         # create thena functions
-        f_model, f_residual, f_objective, f_jacobian, f_stats = fitting.make_multi_gaussian_model()
+        f_model, f_residual, f_objective, f_jacobian, f_stats = make_multi_gaussian_model()
 
         # read file
         hpxfile = fits.getdata(filename, ext=1)
 
         def fit_spectra():
-            for y in hpxfile['DATA']:
+            for y in hpxfile['DATA'][:1]:
                 yield fit_spectrum(y, f_objective, f_jacobian, f_stats, p)
 
         results = {k : v for k, v in it.izip(hpxfile['HPXINDEX'], fit_spectra())}
@@ -36,7 +37,7 @@ def fit_file(filename, p):
 
 def gen_file_fit():
     # get filenames
-    filenames = glob.glob('/users/dlenz/projects/gaussdec/ebhis_hpx_1024*141of192_G7.fits')
+    filenames = glob.glob('/users/dlenz/projects/gaussdec/ebhis_hpx_1024*112of192_G7.fits')
     
     # set default parameters
     p = default_p
