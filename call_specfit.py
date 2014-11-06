b1539f431f9151ccb57125bac88bdc3c3ce6e794
Daniel Lenz <dlenz.bonn@gmail.com>
2014-11-06 09:56:24 +0100

    Major improvements to call_specfit, read and write into hdf5 files.

--- call_specfit.py
+++ call_specfit.py
@@ -1,3 +1,4 @@
+from functools import partial
 from multiprocessing import Pool
 import argparse
 import os
@@ -12,50 +13,17 @@ from astropy.io import fits
 
 from specfitting import fit_spectrum, make_multi_gaussian_model, default_p
 
-
-def fit_file(args):
-
-    filename, p = args
-
-    print filename, p
-    # create theano functions
-    f_model, f_residual, f_objective, f_jacobian, f_stats = make_multi_gaussian_model()
-
-    # read file
-    hpxfile = fits.getdata(filename, ext=1)
-
-    def fit_spectra():
-        for i, row in enumerate(hpxfile):
-            # print '{n} of {tot}'.format(n=i, tot=len(hpxfile))
-
-            theta, glon = np.rad2deg(hp.pix2ang(1024, row['HPXINDEX']))
-            glat = 90. - theta
-            yield int(row['HPXINDEX']), fit_spectrum(row['DATA'], f_objective, f_jacobian, f_stats, p)
-
-    # put results into dict, dump them to disk
-    results = {int(k): v for k, v in fit_spectra()}
-
-    return results
-
-
-def gen_file_fit(args):
-    # get filenames
-    filenames = glob.glob(args.glob)[:5]
-
-    # set default parameters
-    p = default_p
-    p['clobber'] = False
-    p['survey'] = args.survey
-
-    for filename, p in it.izip(filenames, it.repeat(p)):
-        yield filename, p
+# ebhis standard spectral restframe
+CRPIX3 = 471.921630003202
+CDELT3 = 1288.23448620083
+chan2velo = lambda c: (c - CRPIX3) * CDELT3
 
 
 def create_tables(args):
 
     # append to an existing h5file
     if (os.path.exists(args.outname)):
-        warnings.warn('File {} already exists, will add entries.'.format(args.outname))
+        warnings.warn('File {} already exists!'.format(args.outname))
 
         return
 
@@ -65,7 +33,7 @@ def create_tables(args):
 
         class GaussDec(tables.IsDescription):
             # coordinates
-            index = tables.Int32Col()
+            hpxindex = tables.Int32Col()
             glon = tables.Float32Col()
             glat = tables.Float32Col()
 
@@ -79,48 +47,82 @@ def create_tables(args):
             width_kms = tables.Float32Col()
     
     with tables.open_file(args.outname, mode="w", title="Gaussdec") as fobj:
-            fobj.create_table(fobj.root, 'gaussdec_ebhis', GaussDec, "Gauss decomposition EBHIS")
-            fobj.create_table(fobj.root, 'gaussdec_gass', GaussDec, "Gauss decomposition GASS")
+            ebhis = fobj.create_table(fobj.root, 'gaussdec_ebhis', GaussDec, "Gauss decomposition EBHIS")
+            ebhis.cols.hpxindex.create_csindex()
+            ebhis.autoindex = True
 
+            # gass = fobj.create_table(fobj.root, 'gaussdec_gass', GaussDec, "Gauss decomposition GASS")
+            # gass.cols.hpxindex.create_csindex()
+            
     return 0
 
 
-def fit_files(args):
+def initializer(infile):
+    
+    global f_model, f_residual, f_objective, f_jacobian, f_stats
+
+    # create theano functions
+    f_model, f_residual, f_objective, f_jacobian, f_stats = make_multi_gaussian_model()
+
+    global ebhis_file
+    ebhis_file = tables.open_file(infile, mode="r", title="EBHIS")
+
+
+def do_fit(row_index):
+
+    row = ebhis_file.root.ebhis[row_index]
+    fitresults = fit_spectrum(row['DATA'], f_objective, f_jacobian, f_stats, default_p)['parameters']
+
+    return row_index, fitresults
+
+
+def get_row_index(table):
+    for row_index in range(table.nrows):
+        if not (row_index % 10000):
+            print 'Working on row {i} of {n}...'.format(i=row_index, n=table.nrows)
+        yield row_index
+
+
+def fit_spectra(args):
+
     # create a pool, fit all files
-    with tables.open_file(args.outname, mode="a", title="Gaussdec") as fobj:
-        if args.survey == 'EBHIS':
-            table = fobj.root.gaussdec_ebhis
+    with tables.open_file(args.outname, mode="a", title="Gaussdec") as gaussdec_file:
+        gaussdec_table = gaussdec_file.root.gaussdec_ebhis
+        
+        p = Pool(32, initializer=initializer, initargs=(args.infile,))
+        
+        ebhis_smile = tables.open_file(args.infile, mode="r", title="EBHIS")
+        ebhis_table = ebhis_smile.root.ebhis
 
-        if args.survey == 'GASS':
-            table = fobj.root.gaussdec_gass
+        for row_index, fitresults in p.imap(do_fit, get_row_index(ebhis_table)): 
+            
+            hpxindex = ebhis_table[row_index]['HPXINDEX']
+            glon = ebhis_table[row_index]['GLON']
+            glat = ebhis_table[row_index]['GLAT']
 
-        p = Pool()
-        for fitresults in p.imap(fit_file, gen_file_fit(args)):
-            for k, v in fitresults.iteritems():
-                theta, glon = np.rad2deg(hp.pix2ang(1024, int(k)))
-                glat = 90. - theta
-                for i in range(len(v['parameters'])/3):
-                    entry = table.row
-                    entry['index'] = int(k)
-                    entry['glon'] = glon
-                    entry['glat'] = glat
+            for i in range(len(fitresults)/3):
+                entry = gaussdec_table.row
+                entry['hpxindex'] = hpxindex
+                entry['glon'] = glon
+                entry['glat'] = glat
 
-                    entry['amplitude'] = v['parameters'][i*3]
-                    entry['center_c'] = v['parameters'][i*3 + 1]
-                    entry['center_kms'] = np.nan
+                entry['amplitude'] = fitresults[i * 3]
+                entry['center_c'] = fitresults[i * 3 + 1]
+                entry['center_kms'] = chan2velo(entry['center_c'])
 
-                    entry['width_c'] = v['parameters'][i*3 + 2]
-                    entry['width_kms'] =  np.nan
+                entry['width_c'] = fitresults[i * 3 + 2]
+                entry['width_kms'] =  entry['width_c'] * CDELT3
 
-                    entry.append()
-        table.cols.index.create_csindex()
-        table.flush()
+                entry.append()
+                        
+        gaussdec_table.flush()
 
     return 0
+
 
 # main
 if __name__ == '__main__':
-    
+
     # evaluate parsed arguments
     argp = argparse.ArgumentParser(description='Generate a Gauss decomposition of the full EBHIS/GASS sky.')
     argp.add_argument('-s', '--survey',
@@ -129,10 +131,10 @@ if __name__ == '__main__':
                 choices = ['GASS', 'EBHIS'],
                 help='Survey that is used for the decomposition.',
                 type=str)
-    argp.add_argument('-g', '--glob',
-                default='/vol/arc1/archive/bwinkel/EBHIShpx/ebhis_hpx_1024*of192_G7.fits',
-                metavar='glob pattern',
-                help='Glob pattern to find the source hpx-files.',
+    argp.add_argument('-i', '--infile',
+                default='/vol/ebhis2/data1/dlenz/projects/ebhis2pytable/data/ebhis.h5',
+                metavar='infile',
+                help='Source pytable.',
                 type=str)
     argp.add_argument('outname',
                 metavar='output_filename',
@@ -149,7 +151,7 @@ if __name__ == '__main__':
     create_tables(args)
     
     # fit files
-    fit_files(args)
+    fit_spectra(args)
 
 
 
