d05b76de053f9bc5b5764f4301e6a6c6c4c5a9d6
Daniel Lenz <dlenz.bonn@gmail.com>
2015-01-06 10:59:05 +0100

    Finish the major part of the script.

--- inspect_gaussdec.py
+++ inspect_gaussdec.py
@@ -1,8 +1,18 @@
+"""
+Inspect the Gaussian decomposition of EBHIS and GASS
+
+Functions
+---------
+
+inspect_spectra(data_table, model_table, nsamples) : Inspect a given, random
+    number of spectra
+"""
+
 import tables
 import numpy as np
 import pylab as pl
+import argparse
 
-from astropy.io import fits
 import healpy as hp
 
 from specfitting import make_multi_gaussian_model
@@ -10,27 +20,37 @@ from specfitting import make_multi_gaussian_model
 # convert channels to m/s
 CRPIX3 = 471.921630003202
 CDELT3 = 1288.23448620083
-chan2velo = lambda c: (c - CRPIX3) * CDELT3
+
+def chan2velo(channel):
+    """
+    Convert EBHIS channel number to LSR velocity in m/s
+    """
+    return (channel - CRPIX3) * CDELT3
+
 
 def reconstruct_coldens(table):
-
+    """
+    Reconstruct a column density map of the full sky at nside=1024
+    """
     npix = hp.nside2npix(2**10)
     hi_model = np.zeros(npix, dtype=np.float32)
-    
+
     for row in table:
         hi_model[row['hpxindex']] += row['amplitude']
 
     # convert to cm**-2, 1.288 is EBHIS chanwidth
     to_coldens = 1.82e18 * 1.288
-    
+
     return hi_model * to_coldens
 
 
 def make_ncomp_map(table):
-
+    """
+    Create a map of the number of components, used to model the HI emission
+    """
     npix = hp.nside2npix(2**10)
-    ncomps = np.zeros(npix, dtype=np.float32)
-    
+    ncomps = np.zeros(npix, dtype=int)
+
     for row in table:
         ncomps[row['hpxindex']] += 1
 
@@ -38,20 +58,23 @@ def make_ncomp_map(table):
 
 
 def inspect_spectra(data_table, model_table, nsamples, x_model):
+    """
+    Inspect a given, random number of spectra
+    """
+    model_functions = make_multi_gaussian_model()
+    f_model = model_functions[0]
 
-    f_model, f_residual, f_objective, f_jacobian, f_stats = make_multi_gaussian_model()
-    
     # draw random, unique hpxindices
-    indices = model_table.cols.hpxindex[:]
-    sample_indices = np.random.choice(np.unique(indices), size=nsamples, replace=False)
+    indices = np.unique(model_table.cols.hpxindex[:])
+    sample_indices = np.random.choice(indices, size=nsamples, replace=False)
 
     spectra = []
     model_spectra = []
-    
     for sample_index in sample_indices:
         # data
-        spectra.append(np.squeeze(data_table.read_where("""HPXINDEX=={}""".format(sample_index))['DATA']))
-        
+        spectra.append(np.squeeze(
+            data_table.read_where("""HPXINDEX=={}""".format(sample_index))['DATA']))
+
         # model
         gauss_params = np.array([[row['amplitude'], row['center_kms'], row['width_kms']] for row in model_table.where("""hpxindex=={}""".format(sample_index))])
         model_spectra.append(f_model(gauss_params.flatten(), x_model)[1])
@@ -59,49 +82,62 @@ def inspect_spectra(data_table, model_table, nsamples, x_model):
     return spectra, model_spectra
 
 
-    # read spectra from data and evalutate model
-    x = np.linspace(0., 945., 945)
-    with fits.open('data/ebhis_hpx_1024_hpxpix015of192_G7.fits') as hdulist:
-        hpxfile = hdulist[1].data
-
-        spectra = []
-        model_spectra = []
-        for sample_index in sample_indices:
-            # data
-            spectra.append(np.squeeze(hpxfile['DATA'][hpxfile['HPXINDEX'] == sample_index]))
-            # model
-            gauss_params = np.array([[row['amplitude'], row['center_c'], row['width_c']] for row in table.where("""index=={}""".format(sample_index))])
-            model_spectra.append(f_model(gauss_params.flatten(), x)[1])
-
-    return spectra, model_spectra
-
-
-
-def inspect_spectra(data_table, model_table, nsamples):
-
-    return 0
-
-
 def main():
-    gaussdec_file = tables.open_file('data/gaussdec.h5', mode="r", title="Gaussdec")
-    gaussdec_table = gaussdec_file.root.gaussdec_ebhis
+    """
+    Inspect the Gaussian decomposition of EBHIS and GASS
+    """
 
-    ebhis_file = tables.open_file(
-        '/vol/ebhis2/data1/dlenz/projects/ebhis2pytable/data/ebhis.h5',
-        mode="r",
-        title="EBHIS")
-    ebhis_table = ebhis_file.root.ebhis
+    # evaluate parsed arguments
+    argp = argparse.ArgumentParser(description=__doc__)
+
+    argp.add_argument(
+        '-g',
+        '--gaussdec',
+        metavar='gaussdec',
+        help='location of the Gaussian decomposition',
+        type=str)
+
+    argp.add_argument(
+        '-d',
+        '--data',
+        default='/vol/ebhis1/data1/dlenz/projects/ebhis2pytable/data/ebhis.h5',
+        metavar='infile',
+        help='Data pytable',
+        type=str)
+
+    argp.add_argument(
+        '-n',
+        '--nsamples',
+        default=5,
+        metavar='nsamples',
+        help='Number of random sightlines that are inspected',
+        type=int)
+
+    args = argp.parse_args()
+
+    # load tables
+    gdec_store = tables.open_file(args.gaussdec, mode="r", title="Gaussdec")
+    gdec = gdec_store.root.gaussdec_ebhis
+
+    ebhis_store = tables.open_file(
+        args.data,
+        mode="r")
+    ebhis = ebhis_store.root.ebhis
 
     # inspect reconstruction
-    hi_model = reconstruct_coldens(table=gaussdec_table)
-    hp.mollview(hi_model, xsize=4000.)
+    # hi_model = reconstruct_coldens(table=gdec)
+    # hp.mollview(hi_model, xsize=4000.)
 
     # inspect spectra
     x_data = chan2velo(np.arange(945))
     x_model = np.linspace(-500.e3, 500.e3, 1e4)
 
-    spectra, model_spectra = inspect_spectra(data_table=ebhis_table, model_table=gaussdec_table, nsamples=10, x_model=x_model)
-    
+    spectra, model_spectra = inspect_spectra(
+        data_table=ebhis,
+        model_table=gdec,
+        nsamples=10,
+        x_model=x_model)
+
     shift = 0
     for i, (spectrum, model_spectrum) in enumerate(zip(spectra, model_spectra)):
         pl.plot(x_data, spectrum + shift)
